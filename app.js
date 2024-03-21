71a8cf2797a528d68e2aa9064b8ebb8cc3c3ce6f
Muhammad Zubair Babar <zubair.babar79@gmail.com>
2024-03-21 14:56:55 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// Write a loop to print numbers from 1 to 10.
+for (var i = 1; i <=10; i++){
+    console.log(i);
+}
+
+// Write a loop to print even numbers from 1 to 20.
+for (var i = 1; i <= 20;i++ ){
+    if (i%2 == 0){
+        console.log(i);
+    }
+}
+
+// Write a loop to print odd numbers from 1 to 20.
+for (var i = 1; i <= 15; i++){
+    if(i%2 == 1){
+        console.log(i);
+    }
+}
+
+// Write a loop to print the square of numbers from 1 to 10.
+for (var a = 1; a <= 10; a++){
+    console.log( a*a );
+}
+
+// Write a loop to print the reverse of a string (let's say "hello").
+for (var i = 1; i <= 10; i++){
+    console.log("hello " + i);
+}
+
+// Write a loop to print the multiplication table of a number (let's say 7) up to 10
+for(var i = 1; i <= 10; i++){
+    console.log(7 + "x" + i + "="+  i*7);
+}
+
+
+
+
